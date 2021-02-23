a82c0cb89454616d1e098f80b7bc4257dbe3cd5f
bacarini <bacarini.bruno@gmail.com>
2021-02-23 23:37:28 +0000

    fix db issues

--- src/db/index.js
+++ src/db/index.js
@@ -3,13 +3,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 })
 
+const query = async (text, params) => {
+  const res = await pool.query(text, params)
+  return res
+}
+
 module.exports = {
-  query: (text, params, callback) => {
-    const start = Date.now()
-    return pool.query(text, params, (err, res) => {
-      const duration = Date.now() - start
-      console.log('executed query', { text, duration, rows: res.rowCount })
-      callback(err, res)
-    })
-  },
+  query
 }
--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 const scrape = require('./spiders/continente')
 const db = require('./db')
 
-console.log(process.argv)
 var productsToSearch = process.argv.slice(2);
 const main = async (productsToSearch) => {
   await Promise.all(
     productsToSearch.map(async (productToSearch) => {
       console.log(`Start looking for ${productToSearch}`)
       const data = await scrape(productToSearch)
+
       data.forEach((item) => {
         const values = [item.productName, item.productURL, item.productBrand, item.productPrice, item.productPricePerKg];
         db.query('INSERT INTO products(name, url, brand, price, price_per_kilo) VALUES($1, $2, $3, $4, $5) RETURNING *', values)
           .then(res => console.log(res.rows[0]))
           .catch(e => console.error(e.stack))
       });
-
     })
   )
 }
